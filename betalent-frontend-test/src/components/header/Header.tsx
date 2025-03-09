c0a47a1140bd88a999cb2dfae8fa42e399e7b3e4
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2025-03-09 17:34:05 -0300

    Header.tsx component

--- betalent-frontend-test/src/components/header/Header.tsx
+++ betalent-frontend-test/src/components/header/Header.tsx
@@ -1,1 +1,13 @@
-// TODO
+import logotipo from '../../assets/img/betalent-logo.svg';
+
+const Header = () => {
+    return (
+        <header className="h-[60px] bg-white flex items-center shadow-[0px_2px_8px_0px_rgba(0,0,0,0.2)]">
+            <div className="p-[10px] pl-[20px]">
+                <img src={logotipo} alt="Logotipo Betalent" className="h-[34px] w-[91.9px]" />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
